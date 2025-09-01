b4fc5b6d8926e823bbacfecf36bfbe13ad713d00
น้ำผึ้ง <Nampungdee5555@gmail.com>
2025-09-01 09:33:03 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,11 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwLHCTi5dk5u2UWxPxk4ON-iQCJdFxZLMPT0j-3tv8vHP0iWuE4R51ismQOpUxVl9XJbA/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbz_R_Z3L0iKCYSmTnsf_d2qC859S79OGiYEq_pOcTAyT52zs_lO1uvP3aPdvvwxX3fn0A/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxazQLh5ipkbqoJOjS-htast7iRF6DHqU3hAcVePQWdhivh7x7kf1KRDNvsDE3impYnhQ/exec';
 //LIFF_ID
     const LIFF_ID = '2007868023-VD36zPx6'; // LIFF ID Checkin
     const LIFF_HISTORY = '2007868023-z4ZY4g9Y'; // LIFF ID history
     const LIFF_REGISTER = '2007868023-QvDrAKlr'; // LIFF ID register
+
 
 
 
